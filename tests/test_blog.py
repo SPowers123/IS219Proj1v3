d3910ed8542e7943b4e607a9ec4ac1174418daef
SPowers123 <79128221+SPowers123@users.noreply.github.com>
2022-03-10 01:26:47 -0500

    Update test_blog.py

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -3,9 +3,10 @@ import pytest
 from flaskApp.db import get_db
 
 
+    
 def test_index(client, auth):
     response = client.get("/")
-    assert b"Log In" in response.data
+    assert b"Login" in response.data
     assert b"Register" in response.data
 
     auth.login()
