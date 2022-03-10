12980971fb5668499630aca8635c53cb7636104b
SPowers123 <79128221+SPowers123@users.noreply.github.com>
2022-03-09 23:10:12 -0500

    Add files via upload

---
+++ tests/simple_pages_test.py
@@ -0,0 +1,27 @@
+"""This test the homepage"""
+
+def test_request_main_menu_links(client):
+    """This makes the index page"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b'href="/page/about"' in response.data
+    assert b'href="/page/welcome"' in response.data
+
+def test_request_about(client):
+    """This makes the index page"""
+    response = client.get("/page/about")
+    assert response.status_code == 200
+    assert b"About" in response.data
+
+def test_request_welcome(client):
+    """This makes the index page"""
+    response = client.get("/page/welcome")
+    assert response.status_code == 200
+    assert b"Welcome" in response.data
+
+
+def test_request_page_not_found(client):
+    """This makes the index page"""
+    response = client.get("/page/page5")
+    assert response.status_code == 404
+
